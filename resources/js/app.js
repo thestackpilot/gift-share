5f69e9ae50fde14de042a8691f8cc05e13558aec
Waleed Bhatti <waleedbhatti@Adils-MacBook-Pro.local>
2025-12-02 16:09:52 +0500

    added auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap';
-import * as bootstrap from 'bootstrap';
 
-// Make Bootstrap available globally
-window.bootstrap = bootstrap;
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
